f994b1f75e71e924b50eb55298145ca9844d008f
rubenavaldez <rubenavaldez@yahoo.com>
2024-10-14 20:12:11 -0700

    added mutations

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,18 @@ import { PetsModule } from './pets/pets.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(process.cwd(), 'data/database.sqlite'),
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     PetsModule,
   ],
--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,11 +1,18 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Args, Resolver, Query, Mutation } from '@nestjs/graphql';
 import { PetsService } from './pets.service';
 import { Pet } from './pet.entity';
+import { CreatePetInput } from './dto/create-pet.input';
 @Resolver((of) => [Pet])
 export class PetsResolver {
   constructor(private petsService: PetsService) {}
   @Query((returns) => [Pet])
   pets(): Promise<Pet[]> {
     return this.petsService.findAll();
+  }
+  @Mutation((returns) => Pet)
+  createPet(
+    @Args('createPetInput') createPetInput: CreatePetInput,
+  ): Promise<Pet> {
+    return this.petsService.createPet(createPetInput);
   }
 }
